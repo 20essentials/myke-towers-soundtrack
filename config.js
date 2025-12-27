d396570bd19a936cab06ade052df5f8dadafec32
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:14:23 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MYKE TOWERS';
+export let nameSongs = [
+	"LA FORMA EN QUE ME MIRAS", /*1*/
+	"SI SE DA", /*2*/
+	"BELLAQUITA", /*3*/
+	"ULALA ", /*4*/
+	"AGUARDIENTE", /*5*/
+	"MI DROGA", /*6*/
+	"VOODOO", /*7*/
+	"CENIZAS QUEDAN", /*8*/
+	"LO QUE PIDE", /*9*/
+	"EN ALTA", /*10*/
+	"LO LOGRÉ", /*11*/
+	"LALA", /*12*/
+	"LA PLAYA (REMIX)", /*13*/
+	"SI SE DA", /*14*/
+	"EXTASY", /*15*/
+	"EXPLÍCITO", /*16*/
+	"ADIVINO", /*17*/
+	"LA CAPI", /*18*/
+	"COSMETICOS", /*19*/
+	"CUANDO BEBE", /*20*/
+]
